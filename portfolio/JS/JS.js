f89bd9dbc3697f9297c42a35860e4edcc7e469e6
Daniil <danekmix@mail.ru>
2022-01-22 19:02:34 +0300

    feat: portfolio functionality added

--- portfolio/JS/JS.js
+++ portfolio/JS/JS.js
@@ -16,3 +16,23 @@ window.addEventListener('resize',() => {
     toggleMenu()
   }
 })
+
+
+// Смена картинок 
+const buttons = document.querySelectorAll('.Portfolio__menu-item')
+const photos = document.querySelectorAll('.photo')
+buttons.forEach(button => {
+  button.addEventListener('click', () =>{
+    for(let j = 0; j < buttons.length;j++){
+      if(buttons[j] != buttons[button]){
+        buttons[j].classList.remove('active-button');
+      }
+    }
+    let season = button.getAttribute("data-season")
+    button.classList.add('active-button');
+
+    for(let i = 0; i < photos.length; i++){
+      photos[i].src = `IMG/${season}/${i + 1}.jpg`
+    }
+  })
+})
